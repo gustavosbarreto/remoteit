49cea37d24cba29e8b5103699ea748b5cde0ec4d
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2019-01-24 17:59:51 -0200

    Initial commit

---
+++ agent/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	shellwords "github.com/mattn/go-shellwords"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use: "ssh-tunnel",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	viper.AutomaticEnv()
+
+	deviceID := getDeviceID(viper.GetString("DEVICE_ID"))
+	mqttServer := viper.GetString("MQTT_SERVER")
+	sshServer := viper.GetString("SSH_SERVER")
+	sshPort := viper.GetString("SSH_PORT")
+	privateKey := viper.GetString("PRIVATE_KEY")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+
+	helpCalled, err := rootCmd.Flags().GetBool("help")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	if helpCalled {
+		os.Exit(1)
+	}
+
+	cmds := make(map[int]*exec.Cmd)
+
+	fmt.Printf("device-id=%s\n", deviceID)
+
+	opts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", mqttServer))
+	opts.SetAutoReconnect(true)
+	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
+		connect(client)
+	})
+	opts.SetOnConnectHandler(func(client mqtt.Client) {
+		if token := client.Subscribe(fmt.Sprintf("connect/%s", deviceID), 0, func(client mqtt.Client, msg mqtt.Message) {
+			go func() {
+				parts := strings.SplitN(string(msg.Payload()), ":", 2)
+				port, _ := strconv.Atoi(parts[0])
+
+				fmt.Printf("reverse port=%d\n", port)
+
+				args := []string{
+					"ssh",
+					"-i", privateKey,
+					"-o", "StrictHostKeyChecking=no",
+					"-nNT",
+					"-p", sshPort,
+					"-R", fmt.Sprintf("%d:localhost:22", port),
+					fmt.Sprintf("%d:%s@%s", port, parts[1], sshServer),
+				}
+
+				cmd := exec.Command(args[0], args[1:]...)
+				_ = cmd.Start()
+
+				cmds[port] = cmd
+			}()
+		}); token.Wait() && token.Error() != nil {
+			log.Fatal(token.Error())
+		}
+
+		if token := client.Subscribe(fmt.Sprintf("disconnect/%s", deviceID), 0, func(client mqtt.Client, msg mqtt.Message) {
+			port, _ := strconv.Atoi(string(msg.Payload()))
+
+			if cmd, ok := cmds[port]; ok {
+				cmd.Process.Kill()
+				cmd.Wait()
+				delete(cmds, port)
+			}
+		}); token.Wait() && token.Error() != nil {
+			log.Fatal(token.Error())
+		}
+	})
+
+	client := mqtt.NewClient(opts)
+
+	connect(client)
+
+	select {}
+}
+
+func getDeviceID(deviceID string) string {
+	parts := strings.Split(deviceID, ":")
+	if len(parts) < 2 {
+		return deviceID
+	}
+
+	switch parts[0] {
+	case "value":
+		return strings.Join(parts[1:], ":")
+	case "exec":
+		args, err := shellwords.Parse(strings.Join(parts[1:], ":"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var out bytes.Buffer
+
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = &out
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		return strings.TrimSpace(out.String())
+	}
+
+	return deviceID
+}
+
+func connect(client mqtt.Client) {
+	for {
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		break
+	}
+}
---
+++ server/ssh-server/server.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	sshserver "github.com/gliderlabs/ssh"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Server struct {
+	broker     mqtt.Client
+	sshd       *sshserver.Server
+	opts       *Options
+	channels   map[uint32]chan bool
+	forwarding map[uint32]string
+}
+
+func NewServer(opts *Options) *Server {
+	s := &Server{
+		opts:       opts,
+		channels:   make(map[uint32]chan bool),
+		forwarding: make(map[uint32]string),
+	}
+
+	s.sshd = &sshserver.Server{
+		Addr:             opts.Addr,
+		PublicKeyHandler: s.publicKeyHandler,
+		Handler:          s.sessionHandler,
+		ReversePortForwardingCallback: s.reversePortForwardingHandler,
+	}
+
+	if _, err := os.Stat("private.key"); os.IsNotExist(err) {
+		logrus.Fatal("Private key not found!")
+	}
+
+	s.sshd.SetOption(sshserver.HostKeyFile("private.key"))
+
+	bopts := mqtt.NewClientOptions().AddBroker(opts.Broker)
+	bopts.SetAutoReconnect(true)
+	bopts.SetOnConnectHandler(func(client mqtt.Client) {
+		logrus.WithFields(logrus.Fields{
+			"broker": s.opts.Broker,
+		}).Info("Successfully connected to broker")
+	})
+	bopts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
+		logrus.WithFields(logrus.Fields{
+			"broker": s.opts.Broker,
+			"err":    err,
+		}).Error("Lost connection from broker")
+
+		s.broker = client
+
+		s.connectToBroker()
+	})
+
+	s.broker = mqtt.NewClient(bopts)
+
+	return s
+}
+
+func (s *Server) sessionHandler(session sshserver.Session) {
+	logrus.WithFields(logrus.Fields{
+		"target":  session.User(),
+		"session": session.Context().Value(sshserver.ContextKeySessionID),
+	}).Info("Handling session request")
+
+	sess, err := NewSession(session.User(), session)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Error(err)
+
+		io.WriteString(session, fmt.Sprintf("%s\n", err))
+		session.Close()
+		return
+	}
+
+	sess.port, err = s.nextAvailablePort()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Error("No available ports")
+
+		io.WriteString(session, "No available ports\n")
+		session.Close()
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"target":   sess.target,
+		"username": sess.user,
+		"port":     sess.port,
+		"session":  session.Context().Value(sshserver.ContextKeySessionID),
+	}).Info("Session created")
+
+	if _, ok := s.channels[sess.port]; !ok {
+		s.channels[sess.port] = make(chan bool)
+	}
+
+	fwid, err := uuid.NewV4()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"err": err,
+		}).Error("Failed to generate forward id")
+		session.Close()
+		return
+	}
+
+	s.forwarding[sess.port] = fmt.Sprintf("%d:%s", sess.port, fwid.String())
+
+	s.publish("connect", sess.target, fmt.Sprintf("%d:%s", sess.port, fwid.String()))
+
+	select {
+	case <-s.channels[sess.port]:
+		logrus.WithFields(logrus.Fields{
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Info("Reverse port forwarding client connected")
+	case <-time.After(s.opts.ConnectTimeout):
+		logrus.WithFields(logrus.Fields{
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Error("Timeout waiting for reverse port forward client")
+
+		io.WriteString(session, "Timeout\n")
+		session.Close()
+		return
+	}
+
+	passwd, err := sess.promptForPassword()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Error("Failed to read password")
+
+		io.WriteString(session, "Failed to read password\n")
+		session.Close()
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"session": session.Context().Value(sshserver.ContextKeySessionID),
+	}).Info("Forwarding session to device")
+
+	err = sess.connect(passwd)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"err":     err,
+			"session": session.Context().Value(sshserver.ContextKeySessionID),
+		}).Error("Failed to estabilish connection to device")
+
+		io.WriteString(session, "Failed to establish connection to device\n")
+		session.Close()
+
+		return
+	}
+
+	delete(s.channels, sess.port)
+
+	s.publish("disconnect", sess.target, fmt.Sprintf("%d", sess.port))
+}
+
+func (s *Server) connectToBroker() {
+	logrus.WithFields(logrus.Fields{
+		"broker": s.opts.Broker,
+	}).Info("Connecting to broker")
+
+	for {
+		if token := s.broker.Connect(); token.Wait() && token.Error() != nil {
+			logrus.WithFields(logrus.Fields{
+				"broker": s.opts.Broker,
+				"err":    token.Error(),
+			}).Error("Failed to connect to broker")
+
+			time.Sleep(time.Second * 10)
+		} else {
+			break
+		}
+	}
+}
+
+func (s *Server) publicKeyHandler(ctx sshserver.Context, key sshserver.PublicKey) bool {
+	return true
+}
+
+func (s *Server) reversePortForwardingHandler(ctx sshserver.Context, host string, port uint32) bool {
+	if host != "localhost" {
+		logrus.WithFields(logrus.Fields{
+			"host": host,
+			"port": port,
+			"user": ctx.User(),
+		}).Error("Invalid host")
+
+		return false
+	}
+
+	if port < s.opts.MinPort || port > s.opts.MaxPort {
+		logrus.WithFields(logrus.Fields{
+			"host": host,
+			"port": port,
+			"user": ctx.User(),
+		}).Error("Port out of range")
+
+		return false
+	}
+
+	if fwid, ok := s.forwarding[port]; !ok || fwid != ctx.User() {
+		logrus.WithFields(logrus.Fields{
+			"host": host,
+			"port": port,
+			"user": ctx.User(),
+		}).Error("Forwarding not authorized")
+
+		return false
+	}
+
+	delete(s.forwarding, port)
+
+	if _, ok := s.channels[port]; ok {
+		s.channels[port] <- ok
+	}
+
+	return true
+}
+
+// publish publishes a `message` on `topic/target` to broker
+func (s *Server) publish(topic, target, message string) error {
+	logrus.WithFields(logrus.Fields{
+		"topic":   topic,
+		"target":  target,
+		"message": message,
+	}).Info("Publish to broker")
+
+	topic = fmt.Sprintf("%s/%s", topic, target)
+	if token := s.broker.Publish(topic, 0, false, message); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	return nil
+}
+
+// nextAvailableport returns the next available free port on host
+func (s *Server) nextAvailablePort() (uint32, error) {
+	ln, err := net.Listen("tcp", "[::]:0")
+	if err != nil {
+		return 0, err
+	}
+
+	return uint32(ln.Addr().(*net.TCPAddr).Port), ln.Close()
+}
+
+func (s *Server) ListenAndServe() error {
+	s.connectToBroker()
+
+	logrus.WithFields(logrus.Fields{
+		"addr": s.opts.Addr,
+	}).Info("SSH server listening")
+
+	return s.sshd.ListenAndServe()
+}
