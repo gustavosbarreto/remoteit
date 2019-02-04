8f7b14ad8323bbee944a1eb9e16dea231c0f8fdf
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-02-04 16:40:29 -0200

    Show devices

--- server/ui/src/components/HelloWorld.vue
+++ server/ui/src/components/HelloWorld.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-container>ae</v-container>
+  <v-container>
+    <v-expansion-panel>
+      <v-expansion-panel-content v-for="device in devices" :key="device.uid">
+        <div slot="header">{{ device.uid }}</div>
+        <v-card>
+          <v-card-text>
+            <code>{{ JSON.stringify(device.identity, null, 4) }}</code>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-layout align-center justify-end>
+              <v-card-text class="grey--text darken-1">Ultima vez online: {{ device.last_seen }}</v-card-text>
+              <v-btn flat color="orange">Open Terminal</v-btn>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-container>
 </template>
 
 <script>
@@ -9,14 +27,28 @@ export default {
       devices: []
     };
   },
+
   async mounted() {
-    this.devices = await this.$http.get("/api/devices");
+    this.devices = await this.getDevices();
+  },
+
+  methods: {
+    async getDevices() {
+      return await this.$http.get("/api/devices").then(res => {
+        return res.data;
+      });
+    }
   }
 };
 </script>
 
 <style scoped>
-body {
-  color: red;
+code {
+  width: 100%;
+  padding: 20px;
+}
+
+code::before {
+  content: "";
 }
 </style>
