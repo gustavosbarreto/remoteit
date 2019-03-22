ba8d19f0d32c25968270ac41e7c89e931534d064
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-03-22 18:20:35 -0300

    web: list devices

--- server/web/src/views/Devices.vue
+++ server/web/src/views/Devices.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h1>Devices</h1>
+    <v-container grid-list-xl fluid>
+      <v-layout row wrap>
+        <v-flex sm12>
+          <h1>Devices</h1>
+        </v-flex>
+        <v-flex lg12>
+          <v-data-table :headers="headers" :items="devices" class="elevation-1">
+            <template v-slot:items="props">
+              <td>{{ props.item.uid }}</td>
+              <td>{{ props.item.public_key }}</td>
+            </template>
+          </v-data-table>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -8,8 +22,23 @@
 import ApiService from "@/services/api";
 
 export default {
-  created() {
-    ApiService.get("/devices");
+  data() {
+    return {
+      headers: [
+        {
+          text: "UID",
+          align: "left",
+          sortable: false,
+          value: "uid"
+        },
+        { text: "Public Key", sortable: false, value: "public_key" }
+      ],
+      devices: []
+    };
+  },
+
+  async created() {
+    this.devices = await ApiService.get("/devices").then(res => res.data);
   }
 };
 </script>
