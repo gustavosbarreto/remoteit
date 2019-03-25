3581f6db5bad713b009c7144d93c569b2b2bafb5
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2019-03-25 14:01:08 -0300

    web: Show device mac

--- server/web/src/views/Devices.vue
+++ server/web/src/views/Devices.vue
@@ -6,10 +6,10 @@
           <h1>Devices</h1>
         </v-flex>
         <v-flex lg12>
-          <v-data-table :headers="headers" :items="devices" class="elevation-1">
+          <v-data-table :headers="headers" :items="devices" hide-actions="true" class="elevation-1">
             <template v-slot:items="props">
               <td>{{ props.item.uid }}</td>
-              <td>{{ props.item.public_key }}</td>
+              <td>{{ props.item.identity.mac }}</td>
             </template>
           </v-data-table>
         </v-flex>
@@ -31,7 +31,7 @@ export default {
           sortable: false,
           value: "uid"
         },
-        { text: "Public Key", sortable: false, value: "public_key" }
+        { text: "MAC Address", sortable: false, value: "identity.mac" }
       ],
       devices: []
     };
